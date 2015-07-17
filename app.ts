e4b900e9e65396b0d342ec57baa288aac9f1f886
周捷 <zhoujie@meiriq.com>
2015-07-17 16:16:42 +0800

    添加依赖,完善app.ts

--- app.ts
+++ app.ts
@@ -5,3 +5,13 @@ import express = require("express");
 
 var app=express();
 
+
+app.get("/",function (req,res) {
+	res.send("Hello")
+})
+
+app.listen(3000,function(){
+	console.log("server listen in 3000")
+})
+
+
